2b15773cfe45a71b2d53afc26ab3914eafb5273b
donal <25458345@studentmail.ul.ie>
2026-03-10 22:59:03 +0000

    definition of IDs, creation of incomplete probing function

--- leap.c
+++ leap.c
@@ -1,7 +1,30 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#define VENDOR_ID 0xf182
+#define PRODUCT_ID 0x0003
 static int major;
+
+static struct usb_device_id usb_handles[] = { { USB_DEVICE(VENDOR_ID, PRODUCT_ID) }; };
+MODULE_DEVICE_TABLE(usb, usb_handles);
+
+static int leap_probe(struct usb_interfaces* usb_intfs, struct usb_device_id* usb_dID)
+{
+    struct usb_device* usb_parent = interface_to_usbdev(intf);
+    struct usb_interface* current_intf;
+
+    for (int i = 0; i < udev->config->desc.bNumInterfaces; i++)
+    {
+	current_intf = udev->config->interface[i];
+
+	if (current_intf && current_intf->dev.driver)
+	{
+	    struct usb_driver* old_driver = to_usb_driver(cur_intf->dev.driver);
+
+	    //continue checks here...
+        }
+    }
+}
 
 static ssize_t my_read(struct file *f, char __user *u , size_t l, loff_t *o){
     printk("Read is called\n");
