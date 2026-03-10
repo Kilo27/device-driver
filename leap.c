e72f45c50eb67d1a8eaae7039454ab09d02ef38d
Harry Kennedy <harry.s.kennedy2007@gmail.com>
2026-03-10 17:25:49 +0000

    Added write function

--- leap.c
+++ leap.c
@@ -8,6 +8,27 @@ static ssize_t my_read(struct file *f, char __user *u , size_t l, loff_t *o){
     return 0;
 }
 
+static ssize_t my_write(struct file *filep, const char __user *user_buf, size_t len, loff_t *off){
+    int not_copied, delta, to_copy = (len + *off) < sizeof(text) ? len : (sizeof(text) - *off);
+
+    pr_info("write is called, we want to add %ld bytes, but actually only copying %d bytes. The offset is %lld\n", len, to_copy, *off);
+
+    if(*off >= sizeof(text)){
+        return 0;
+    }
+
+    not_copied = copy_from_user(&text[*off], user_buf, to_copy);
+    delta = to_copy - not_copied;
+
+    if(not_copied){
+        pr_warn("leap.c - Could only copy %d bytes\n",delta);
+
+        *off += delta;
+        return delta;
+    }
+
+}
+
 static struct file_operations fops = {
     .read = my_read
 };
@@ -34,6 +55,6 @@ static void __exit leap_exit(void) {
 module_init(leap_init);
 module_exit(leap_exit);
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("Team-12");
 MODULE_DESCRIPTION("Leap Motion Driver");
