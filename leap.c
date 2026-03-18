8d553f78ca886e2c50532b6944ed27d8518245fe
Kilo27 <kyle.oseoighe@gmail.com>
2026-03-18 13:17:50 +0000

    Fixing leap_probe and leap_disconnect

--- leap.c
+++ leap.c
@@ -7,23 +7,28 @@ static int leap_probe(struct usb_interface *interface, const struct usb_device_i
 static void leap_disconnect(struct usb_interface *interface);
 
 
-static int leap_probe(struct usb_interface *interface, const struct usb_device_id *id) {
-    printk(KERN_INFO "Leap Motion device plugged in\n");
-    // Add your initialization code here
+
+
+static int leap_probe(struct usb_interface *intf, const struct usb_device_id *id) {
+    struct usb_device *udev = interface_to_usbdev(intf);
+    usb_set_intfdata(intf, your_private_data);
     return 0;
 }
 
-static void leap_disconnect(struct usb_interface *interface) {
-    printk(KERN_INFO "Leap Motion device removed\n");
-    // Add your cleanup code here
-}
 
+static void leap_disconnect(struct usb_interface *intf) {
+    struct your_dev *dev = usb_get_intfdata(intf);
+    usb_kill_urb(dev->urb);
+    usb_free_urb(dev->urb);
+}
 
 
 static struct usb_device_id leap_table[] = {
     { USB_DEVICE(0x294B, 0x0001) }, // Leap Motion Vendor/Product ID
     {}
 };
+
+MODULE_DEVICE_TABLE(usb,leap_table);
 
 static struct usb_driver leap_driver = {
     .name = "leap_motion",
