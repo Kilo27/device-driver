bdb939ec5c73bd56ff413d109d30bcc4ee1e8966
Kilo27 <kyle.oseoighe@gmail.com>
2026-03-18 13:54:48 +0000

    Added a Struct we need

--- leap.c
+++ leap.c
@@ -12,10 +12,46 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 
+#define DRIVER_NAME "leap_motion"
+#define LEAP_VENDOR_ID 0x294B
+#define LEAP_PRODUCT_ID 0x0001
+
+#define LEAP_FIFO_FRAMES   8
+
 static int leap_probe(struct usb_interface *interface, const struct usb_device_id *id);
 static void leap_disconnect(struct usb_interface *interface);
 
+struct leap_dev {
+  //Found this online, seems to be standard for linux usb devices using the interface_to_usbdev function - KJ
+  struct usb_device   *udev;
+  struct usb_interface *intf;
 
+  struct urb          *urb;
+  u8                  *urb_buf;
+  dma_addr_t           urb_dma;
+  u8                   ep_addr;
+  int                  ep_interval;
+ 
+  spinlock_t           fifo_lock;
+  struct leap_frame    fifo[LEAP_FIFO_FRAMES];
+  unsigned int         fifo_head;
+  unsigned int         fifo_tail;
+  unsigned int         fifo_count;
+ 
+  wait_queue_head_t    wait;
+ 
+  struct cdev          cdev;
+  int                  minor;
+ 
+  struct mutex         io_mutex;
+  bool                 disconnected;
+ 
+  int                  open_count;
+ 
+  unsigned long        frames_received;
+  unsigned long        frames_dropped;
+  unsigned long        urb_errors;
+};
 
 
 static int leap_probe(struct usb_interface *intf, const struct usb_device_id *id) {
@@ -26,14 +62,14 @@ static int leap_probe(struct usb_interface *intf, const struct usb_device_id *id
 
 
 static void leap_disconnect(struct usb_interface *intf) {
-    struct your_dev *dev = usb_get_intfdata(intf);
+    struct leap_dev *ldev = usb_get_intfdata(intf);
     usb_kill_urb(dev->urb);
     usb_free_urb(dev->urb);
 }
 
 
 static struct usb_device_id leap_table[] = {
-    { USB_DEVICE(0x294B, 0x0001) }, // Leap Motion Vendor/Product ID
+    { USB_DEVICE(LEAP_VENDOR_ID, LEAP_PRODUCT_ID) }, // Leap Motion Vendor/Product ID
     {}
 };
 
