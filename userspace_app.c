c2273a250d5a98a93826faf8b8d11b5b93b73fad
Harry Kennedy <harry.s.kennedy2007@gmail.com>
2026-03-19 14:20:51 +0000

    Userspace reformat with pthread

---
+++ userspace_app.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdint.h>
+
+int main(void){
+
+    pthread_t reader_tid;
+    pthread_t dispatcher_tid;
+
+    
+    printf("UserSpace app started\n");
+    return 0;
+
+
+}
