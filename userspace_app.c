908010344d1b12c352ba5c196f50221c4de6471b
Harry Kennedy <harry.s.kennedy2007@gmail.com>
2026-03-19 14:35:35 +0000

    added open of leapcmd

--- userspace_app.c
+++ userspace_app.c
@@ -11,6 +11,13 @@ int main(void){
     pthread_t reader_tid;
     pthread_t dispatcher_tid;
 
+    cmd_fd = open(CMD_DEVICE, O_RDWR);
+        if(cmd_fd <0){
+            perror("open", CMD_DEVICE);
+            fprintf(stderr, "The module has not been loaded");
+            return 1;
+        }
+
     
     printf("UserSpace app started\n");
     return 0;
