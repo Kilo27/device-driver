e2d185c445792d6bd82a6c1820fd7195d9ab92b3
Harry Kennedy <harry.s.kennedy2007@gmail.com>
2026-03-19 15:23:19 +0000

    added basic gesture support

--- userspace_app.c
+++ userspace_app.c
@@ -16,6 +16,52 @@ struct leap_event{
 };
 
 static const char *gesture_commands[] = {"lsusb","lspci","lsblk", "df -h", "free -h", "uptime", "uname -a", "ip link show"};
+#define numGestures 8
+
+static int cmd_fd;
+
+static int now_ms(void){
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (int)(ts.tv_sec *100 + ts.tv_nsec /1000000);
+}
+
+static int classify_gesture(LEAP_HAND *hand, LEAP_HAND *prev){
+    float grab = hand -> grab_strength;
+    float pinch = hand ->pinch_strength;
+
+    if(pinch > 0.7f){
+        return LEAP_GESTURE_SWIPE_RIGHT;
+    }
+    if(grab > 0.7f){
+        return LEAP_GESTURE_GRAB;
+    }
+    if(grab < 0.15f && pinch < 0.15f){
+        return LEAP_GESTURE_OPEN;
+    }
+
+    if(prev){
+        float handx = hand -> palm.velocity.x;
+        float handy = hand -> palm.velocity.y;
+
+        if(handx > 200.0f){
+            return LEAP_GESTURE_SWIPE_RIGHT;
+        }
+        if(handx < -200.0f){
+            return LEAP_GESTURE_SWIPE_LEFT;
+        }
+        if(handy > 200.0f){
+            return LEAP_GESTURE_SWIPE_UP;
+        }
+         if(handy < -200.0f){
+            return LEAP_GESTURE_SWIPE_DOWN;
+        }
+
+        return -1;
+
+    }
+
+}
 
 int main(void){
 
